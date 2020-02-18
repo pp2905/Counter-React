f67cc9e55d2950e9224ffd88989be66a45f52984
Patryk <patryk.piecek29@gmail.com>
2020-02-18 14:35:19 +0100

    Updates

---
+++ precache-manifest.1322aafdfabddd038cf3d64f1e7d6c53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05339e7809752af252b8e45145fbbc12",
+    "url": "/react-counter/index.html"
+  },
+  {
+    "revision": "66a4e56e00bede4e1716",
+    "url": "/react-counter/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "ff1a3b8da500adab9a23",
+    "url": "/react-counter/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "66a4e56e00bede4e1716",
+    "url": "/react-counter/static/js/2.42242f42.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-counter/static/js/2.42242f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff1a3b8da500adab9a23",
+    "url": "/react-counter/static/js/main.e63715a0.chunk.js"
+  },
+  {
+    "revision": "dff3c4a351ccb19c1365",
+    "url": "/react-counter/static/js/runtime-main.3aba36e4.js"
+  }
+]);
